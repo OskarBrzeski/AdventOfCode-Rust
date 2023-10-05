8577f93701d9d5d53508a2011327d322c0c62095
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-10-05 18:47:32 +0100

    Day 2 Complete

---
+++ src/days/day2.rs
@@ -0,0 +1,28 @@
+use std::fs;
+use std::path::Path;
+
+pub fn solution() -> isize {
+    let file_content = fs::read_to_string(Path::new("data/input2.txt"))
+        .expect("Should be able to read file");
+
+    let lines: Vec<&str> = file_content.split("\r\n").collect();
+
+    let mut score: isize = 0;
+
+    for line in lines {
+        score += match line {
+            "A X" => 4,
+            "A Y" => 8,
+            "A Z" => 3,
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+            "C X" => 7,
+            "C Y" => 2,
+            "C Z" => 6,
+            _ => 0,
+        }
+    }
+    
+    score
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod days;
 fn main() {
     let solutions = vec![
         days::day1::solution(),
+        days::day2::solution(),
     ];
 
     for (s, i) in solutions.iter().zip(1..=25) {
