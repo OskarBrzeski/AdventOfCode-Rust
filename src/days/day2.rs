f52e60f446f3390756287fe006259edff074ec7d
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-10-06 21:30:49 +0100

    Part 2 of Day 2 Done

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,15 +1,20 @@
 use std::fs;
 use std::path::Path;
+use std::str::Split;
 
 pub fn solution() -> (isize, isize) {
     let file_content = fs::read_to_string(Path::new("data/input2.txt"))
         .expect("Should be able to read file");
 
-    let lines: Vec<&str> = file_content.split("\r\n").collect();
+    let lines = file_content.split("\r\n");
+    
+    return (part1(&lines), part2(&lines))
+}
 
+fn part1(lines: &Split<&str>) -> isize {
     let mut score: isize = 0;
 
-    for line in lines {
+    for line in lines.clone().into_iter() {
         score += match line {
             "A X" => 4,
             "A Y" => 8,
@@ -23,6 +28,27 @@ pub fn solution() -> (isize, isize) {
             _ => 0,
         }
     }
-    
-    return (score, 0)
+
+    score
+}
+
+fn part2(lines: &Split<&str>) -> isize {
+    let mut score: isize = 0;
+
+    for line in lines.clone().into_iter() {
+        score += match line {
+            "A X" => 3,
+            "A Y" => 4,
+            "A Z" => 8,
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+            "C X" => 2,
+            "C Y" => 6,
+            "C Z" => 7,
+            _ => -1,
+        }
+    }
+
+    score
 }
