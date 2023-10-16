2f3f4b7b3b1956480252edf504abaaeba6aa126d
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-10-16 21:00:15 +0100

    Print Explicitly

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 mod days;
 
 fn main() {
-    let solutions = vec![
-        days::day1::solution(),
-        days::day2::solution(),
-        days::day3::solution(),
-        days::day4::solution(),
-    ];
-
-    for (s, i) in solutions.iter().zip(1..=25) {
-        println!("Day {}: {:?}", i, s);
-    }
+    println!("Day 1: {:?}", days::day1::solution());
+    println!("Day 2: {:?}", days::day2::solution());
+    println!("Day 3: {:?}", days::day3::solution());
+    println!("Day 4: {:?}", days::day4::solution());
+    // println!("Day 5: {:?}", days::day5::solution());
+    // println!("Day 6: {:?}", days::day6::solution());
+    // println!("Day 7: {:?}", days::day7::solution());
+    // println!("Day 8: {:?}", days::day8::solution());
+    // println!("Day 9: {:?}", days::day9::solution());
+    // println!("Day 10: {:?}", days::day10::solution());
+    // println!("Day 11: {:?}", days::day11::solution());
+    // println!("Day 12: {:?}", days::day12::solution());
+    // println!("Day 13: {:?}", days::day13::solution());
+    // println!("Day 14: {:?}", days::day14::solution());
+    // println!("Day 15: {:?}", days::day15::solution());
+    // println!("Day 16: {:?}", days::day16::solution());
+    // println!("Day 17: {:?}", days::day17::solution());
+    // println!("Day 18: {:?}", days::day18::solution());
+    // println!("Day 19: {:?}", days::day19::solution());
+    // println!("Day 20: {:?}", days::day20::solution());
+    // println!("Day 21: {:?}", days::day21::solution());
+    // println!("Day 22: {:?}", days::day22::solution());
+    // println!("Day 23: {:?}", days::day23::solution());
+    // println!("Day 24: {:?}", days::day24::solution());
+    // println!("Day 25: {:?}", days::day25::solution());
 }
