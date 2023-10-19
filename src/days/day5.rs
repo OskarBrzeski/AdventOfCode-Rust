5e126a7c2eaff84f7485373140ca8db328bf1720
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-10-19 18:44:58 +0100

    Refactor

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -2,31 +2,20 @@ use std::fs;
 use std::path::Path;
 use std::str::{Lines, from_utf8};
 
-pub fn solution() -> (String, i32) {
+pub fn solution() -> (String, String) {
     let file_content = fs::read_to_string(Path::new("data/input5.txt"))
         .expect("Should be able to read files");
 
     let lines = file_content.lines();
 
     (
-        part1(&mut lines.clone()),
-        0,
+        part1(lines.clone()),
+        part2(lines.clone()),
     )
 }
 
-fn part1(lines: &mut Lines) -> String {
-    let mut yeah: Vec<&str> = vec![];
-
-    for _ in 0..8 {
-        yeah.push(lines.next().unwrap());
-    }
-
-    yeah.reverse();
-
-    let mut config = starting_config(yeah);
-
-    lines.next();
-    lines.next();
+fn part1(mut lines: Lines) -> String {
+    let mut config = starting_config(&mut lines);
 
     for line in lines {
         let (amount, from, to) = get_instructions(line);
@@ -39,28 +28,43 @@ fn part1(lines: &mut Lines) -> String {
     let mut answer: [u8; 9] = [0; 9];
     for i in 0..9 {
         let value = config[i].pop().unwrap();
-        answer[i] = *value;
+        answer[i] = value;
     }
 
     String::from(from_utf8(&answer).unwrap())
 }
 
-fn starting_config(lines: Vec<&str>) -> [Vec<&u8>; 9] {
-    let mut config: [Vec<&u8>; 9] = [
+fn part2(mut lines: Lines) -> String {
+    String::new()
+}
+
+fn starting_config(lines: &mut Lines) -> [Vec<u8>; 9] {
+    let mut yeah: Vec<&str> = vec![];
+
+    for _ in 0..8 {
+        yeah.push(lines.next().unwrap());
+    }
+
+    yeah.reverse();
+
+    let mut config: [Vec<u8>; 9] = [
         vec![], vec![], vec![], vec![], vec![], vec![], vec![], vec![], vec![],
     ];
 
-    for line in lines {
-        let line_bytes: Vec<&u8> = line.as_bytes().iter().collect();
+    for line in yeah {
+        let line_bytes = line.as_bytes();
 
         for i in 0..9 {
             let c = line_bytes[4*i+1];
 
-            if *c != 32 {
+            if c != 32 {
                 config[i].push(c);
             }
         }
     }
+
+    lines.next();
+    lines.next();
 
     config
 }
