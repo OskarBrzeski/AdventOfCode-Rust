ec56a5dc64b0fc99594a2e7b08a677976fd77778
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-10-20 15:54:28 +0100

    Day 6 done

---
+++ src/days/day6.rs
@@ -0,0 +1,53 @@
+use std::fs;
+use std::path::Path;
+
+pub fn solution() -> (i32, i32) {
+    let line = fs::read_to_string(Path::new("data/input6.txt"))
+        .expect("Should be able to read file");
+
+    (
+        part1(line.as_bytes()),
+        part2(line.as_bytes()),
+    )
+}
+
+fn part1(line: &[u8]) -> i32 {
+    let mut stack: [u8; 4] = [line[0], line[1], line[2], line[3]];
+
+    for (i, c) in line[4..].iter().enumerate() {
+        stack[i%4] = *c;
+        if all_different(&stack) {
+            return (i+5) as i32
+        }
+    }
+    0
+}
+
+fn part2(line: &[u8]) -> i32 {
+    let mut stack: [u8; 14] = [0; 14];
+    for (i, c) in line[..14].iter().enumerate() {
+        stack[i] = *c;
+    }
+
+    for (i, c) in line[14..].iter().enumerate() {
+        stack[i%14] = *c;
+        if all_different(&stack) {
+            return (i+15) as i32
+        }
+    }
+    0
+}
+
+fn all_different(stack: &[u8]) -> bool {
+    let mut known = [false; 26];
+
+    for i in stack.iter() {
+        let index = (i - 97) as usize;
+        if known[index] {
+            return false
+        }
+        known[index] = true;
+    }
+
+    true
+}
